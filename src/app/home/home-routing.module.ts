ad62a069e1f9246ab91d9a2a7d8d97d4da0ecadf
Mohit Jaiswal <mohitjaiswal2883@gmail.com>
2024-10-27 16:21:00 +0530

    added 404 page to handle page not found

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { PathComponent } from './components/path/path.component';
 import { QueryComponent } from './components/query/query.component';
+import { PageNotFoundComponent } from '../../shared/404/404.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'book/:bookID/author/:author', component: PathComponent },
   { path: 'product', component: QueryComponent },
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
---
+++ src/shared/404/404.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-404',
+  standalone: true,
+  imports: [],
+  templateUrl: './404.component.html',
+})
+
+export class PageNotFoundComponent {
+}
